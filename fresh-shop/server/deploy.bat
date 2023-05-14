f03a9811d4fb6161b9f528e35d5074383d792590
likfees <likfees@gmail.com>
2023-05-14 23:19:07 +0800

    新增部署脚本

--- fresh-shop/server/deploy.bat
+++ fresh-shop/server/deploy.bat
@@ -1,3 +1,5 @@
-ssh root@fungs.cn "rm -rf /www/wwwroot/qiyun/web/*"
-scp -r .\dist\* root@fungs.cn:/www/wwwroot/qiyun/web
+# 部署前记得修改正式配置文件
+
+ssh root@fungs.cn "rm -rf /www/wwwroot/qiyun/server/*"
+scp -r .\* root@fungs.cn:/www/wwwroot/qiyun/server
 pause
