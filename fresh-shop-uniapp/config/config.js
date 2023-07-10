65b0bc1fbb1ea9bc80841b4db0f26971d1618036
likfees <likfees@gmail.com>
2023-07-10 21:52:46 +0800

    修改小程序优惠价显示

--- fresh-shop-uniapp/config/config.js
+++ fresh-shop-uniapp/config/config.js
@@ -13,6 +13,6 @@ export default {
     // 其他使用 url
     // #ifndef H5
      // baseUrl: 'http://localhost:48888',
-    baseUrl: 'https://qiyun.fungs.cn/api',
+    baseUrl: 'https://qiyundongpin.cn/goapi',
     // #endif
 }
